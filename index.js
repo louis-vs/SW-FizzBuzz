f385e27a7829bb3942ae621326a2e0da05add3fe
Louis Van Steene <Louis.VanSteene@softwire.com>
2023-11-08 11:45:55 +0000

    Improved part 1

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-for (let i = 0; i <= 100; i++) {
-    if (i % 15 == 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
-        console.log("Fizz");
-    } else if (i % 5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
+for (let i = 1; i <= 100; i++) {
+    let output = "";
+    if (i % 3 === 0) {
+        output += "Fizz";
+    } 
+    if (i % 5 === 0) {
+        output += "Buzz";
     }
+
+    if (output === "") {
+        output = String(i);
+    }
+
+    console.log(output);
 }
