79846c7ab1cf329fa601fac4909dda4de463b511
Louis Van Steene <Louis.VanSteene@softwire.com>
2023-11-08 12:25:59 +0000

    Complete part 2

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 for (let i = 1; i <= 100; i++) {
-    let output = "";
+    let output = [];
     if (i % 3 === 0) {
-        output += "Fizz";
+        output.push("Fizz");
     } 
 
-    if (i % 13 === 0) {
-        output += "Fezz"
-    }
-
     if (i % 5 === 0) {
-        output += "Buzz";
+        output.push("Buzz");
     }
 
     if (i % 7 === 0) {
-        output += "Bang";
+        output.push("Bang");
     }
 
     if (i % 11 === 0) {
-        output = "Bong";
+        output = ["Bong"];
     }
 
-
-    if (output === "") {
-        output = String(i);
+    if (i % 13 === 0) {
+        let bIndex = output.findIndex((element) => element[0] === "B");
+        if (bIndex === -1) {
+            bIndex = output.length;
+        }
+        output.splice(bIndex, 0, "Fezz");
     }
 
-    console.log(output);
+    if (i % 17 === 0) {
+        output.reverse();
+    }
+
+    if (output.length === 0) {
+        output.push(String(i));
+    }
+
+    console.log(output.join(""));
 }
