a4b2f96bfcb4537a61f18578969b03e188cc2ca3
Louis Van Steene <Louis.VanSteene@softwire.com>
2023-11-08 11:56:58 +0000

    Part 2 up to Fezz

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0) {
         output += "Fizz";
     } 
+
+    if (i % 13 === 0) {
+        output += "Fezz"
+    }
+
     if (i % 5 === 0) {
         output += "Buzz";
     }
+
+    if (i % 7 === 0) {
+        output += "Bang";
+    }
+
+    if (i % 11 === 0) {
+        output = "Bong";
+    }
+
 
     if (output === "") {
         output = String(i);
